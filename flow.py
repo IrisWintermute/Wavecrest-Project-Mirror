6e0eab988f7ce0874a64d4ec0d4e0febbd6f292b
Iris <tomas5french@gmail.com>
2024-05-14 17:06:09 +0100

    added flow outline and function signatures

--- flow.py
+++ flow.py
@@ -7,19 +7,25 @@
 # instance downloads CDR data object from S3 input bucket
 
 # bring data -2D CSV array- into scope
+data_csv = get_data()
 
 # (vectorise) convert each record to array with uniform numerical type - data stored as nested array
+data_array = [[attribute for attribute in record.split(",")].append(0) for record in data_csv.split("\n")]
+for record in data_array:
+    for i, attribute in enumerate(record):
+        if i == 0:
+            attribute = vectorise(attribute, "country")
+        if i == 1:
+            attribute = vectorise(attribute, "region")
+        if i == 2:
+            attribute = vectorise(attribute, "group")
+
+clustered_data_list = []
+for k in range(1, max_k):
+    # run k-means clustering algorithm with vectorised data
+    clustered_data_list = kmeans(k, data_array)
 
 # select optimal k (decision to make on method - depends on data)
-
-# run k-means clustering algorithm with vectorised data
-
-    # use kmeans++ to get initial centroid coordinates
-
-    # assign each data point to closest centroid
-
-    # calculate new centroid coordinates
-
-    # repeat 19 and 21 until no reassignments take place
+(k_optimal, clustered_data) = optimal_k_decision(clustered_data_list)
 
 # push clustered data to S3 output bucket
--- lib.py
+++ lib.py
@@ -1,2 +1,52 @@
 
 # ||function library for k-means program||
+
+# cluster data using k-means algorithm
+def kmeans(k, data_array):
+    # use kmeans++ to get initial centroid coordinates
+    centroids = k_means_pp(k, data_array)
+    centroids_new = centroids
+
+    while True:
+        no_reassignments = True
+        # assign each data point to closest centroid
+        for record in data_array:
+            distances = [(distance_to_centroid(record, centroid), i) for i, centroid in enumerate(centroids)]
+            (_, closest_centroid) = distances.sort(lambda d, _: d)[0]
+            if record[-1] != closest_centroid: 
+                record[-1] = closest_centroid
+                no_reassignments = False
+
+        # stop algorithm when no records are reassigned
+        if no_reassignments: return centroids
+
+        # calculate new centroid coordinates
+        for i in range(centroids):
+            owned_records = [record.pop() for record in data_array if record[-1] == i]
+            centroids_new[i] = average(owned_records)
+
+        centroids = centroids_new
+
+# get data from S3 bucket
+def get_data():
+    pass
+
+# randomly select initial centroids from unclustered data
+def k_means_pp(k, data):
+    pass
+
+# calculate distance between record and centroid
+def distance_to_centroid(record, centroid):
+    pass
+
+# reduce list of input vectors into a single vector representing the average of input vectors
+def average(records):
+    pass
+
+# return optimal k and clustered data from kmeans(k, data)
+def optimal_k_decision(clustered_data_list):
+    pass
+
+# parse non-numeric data into a form that enables vector operations to be performed with data
+def vectorise(attribute):
+    pass
