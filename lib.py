4feba05ad326522e7ddfd0b48c3c19230a1ade87
Iris <tomas5french@gmail.com>
2024-05-15 13:46:10 +0100

    Added bodies to k-means clustering functions.

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
 
 # ||function library for k-means program||
+import random
 
 # cluster data using k-means algorithm
 def kmeans(k: int, data_array: list[list[int]]) -> list[list[int]]:
@@ -11,10 +12,9 @@ def kmeans(k: int, data_array: list[list[int]]) -> list[list[int]]:
         no_reassignments = True
         # assign each data point to closest centroid
         for record in data_array:
-            distances = [(distance_to_centroid(record, centroid), i) for i, centroid in enumerate(centroids)]
-            (_, closest_centroid) = distances.sort(lambda d, _: d)[0]
-            if record[-1] != closest_centroid: 
-                record[-1] = closest_centroid
+            (_, closest_centroid_index) = get_closest_centroid(record, centroids)
+            if record[-1] != closest_centroid_index: 
+                record[-1] = closest_centroid_index
                 no_reassignments = False
 
         # stop algorithm when no records are reassigned
@@ -33,20 +33,49 @@ def get_data() -> str:
 
 # randomly select initial centroids from unclustered data
 def k_means_pp(k: int, data: list[list[int]]) -> list[list[int]]:
-    pass
+    chosen_indexes = [random.randint(0, len(data) - 1)]
+    centroids = [data[chosen_indexes[0]]]
+    square_distances = {}
+
+    while len(centroids) < k:
+        for i, record in enumerate(data):
+            if i not in chosen_indexes:
+                (dist_to_nearest_centroid, _) = get_closest_centroid(record, centroids)
+                square_distances[i] = dist_to_nearest_centroid ** 2
+
+        sum_of_squares = sum(square_distances.values())
+
+        for index, dist in square_distances.items():
+            if random.random() < (dist / sum_of_squares):
+                centroids.append(data[index])
+                break
+
+    return centroids
 
 # calculate distance between record and centroid
-def distance_to_centroid(record: list[int], centroid: list[float]) -> tuple[float, int]:
-    pass
+def distance_to_centroid(record: list[int], centroid: list[float]) -> float:
+    return sum([abs(centroid[i] - attribute) ** 2 for i, attribute in enumerate(record)])
+
+# returns tuple of distance between record and nearest centroid, and index of nearest centroid
+def get_closest_centroid(record: list[int], centroids: list[list[float]]) -> tuple[float, int]:
+    distances = [(distance_to_centroid(record, centroid), i) for i, centroid in enumerate(centroids)]
+    return distances.sort(key = lambda d, _: d)[0]
 
 # reduce list of input vectors into a single vector representing the average of input vectors
 def average(records: list[list[int]]) -> list[float]:
-    pass
+    sum = [0 for _ in records[0]]
+    for record in records:
+        for i, attribute in enumerate(record):
+            sum[i] += attribute
+
+    return [attr / len(sum) for attr in sum]
+    
+
 
 # return optimal k and clustered data from kmeans(k, data)
 def optimal_k_decision(clustered_data_list: list[list[list[int]]]) -> tuple[int, list[list[int]]]:
     pass
 
 # parse non-numeric data into a form that enables vector operations to be performed with data
-def vectorise(attribute: str) -> int:
+def vectorise(attribute: str, label: str) -> int:
     pass
