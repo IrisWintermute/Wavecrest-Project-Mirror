85d4908e281f75472c9e82d07bd90c4d7ef6562d
Matt Haswell <Matt.Haswell@wavecrest.com>
2024-05-15 16:29:30 +0100

    Initial github action and terraform

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "ai-project-vpc" {
+ cidr_block = var.vpc_cidr
+
+ tags = merge(tomap({
+   Name = "AI Project VPC"
+ }), local.common_tags)
+}
+
+# resource "aws_subnet" "public_subnets" {
+#  count             = length(local.vpc_public_subnets)
+#  vpc_id            = aws_vpc.ai-project-vpc.id
+#  cidr_block        = element(local.vpc_public_subnets, count.index)
+#  availability_zone = element(local.azs, count.index)
+
+#  tags = {
+#    Name = "Public Subnet ${count.index + 1}"
+#  }
+# }
+
+resource "aws_subnet" "private_subnets" {
+ count             = length(local.vpc_private_subnets)
+ vpc_id            = aws_vpc.ai-project-vpc.id
+ cidr_block        = element(local.vpc_private_subnets, count.index)
+ availability_zone = element(local.azs, count.index)
+
+ tags = merge(tomap({
+   Name = "Private Subnet ${count.index + 1}"
+ }), local.common_tags)
+}
+
+resource "aws_subnet" "db_subnets" {
+ count             = length(local.vpc_database_subnets)
+ vpc_id            = aws_vpc.ai-project-vpc.id
+ cidr_block        = element(local.vpc_database_subnets, count.index)
+ availability_zone = element(local.azs, count.index)
+
+ tags = merge(tomap({
+   Name = "Database Subnet ${count.index + 1}"
+ }), local.common_tags)
+}
+
+
+
+
+### Required terraform setup ###
+
+terraform {
+  required_version = ">= 1.6.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  backend "s3" {}
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "VPC CIDR"
+  default    = "10.154.0.0/23"
+}
