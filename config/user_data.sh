0a488abb07ae4ac409685f610541020bb2083436
Iris <tomas5french@gmail.com>
2024-05-21 11:30:31 +0100

    added git commands to user_data

--- config/user_data.sh
+++ config/user_data.sh
@@ -2,3 +2,8 @@
 sudo apt update
 sudo apt install python3
 sudo apt install -y python3-pip -y
+sudo apt install git
+
+git remote add ai-project https://github.com/Wavecrest/AI-Project
+git fetch ai-project
+
