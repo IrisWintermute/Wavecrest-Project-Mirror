ff5cea53d8e869017a40f5083571a117e88e58af
James <jg2077@hw.ac.uk>
2024-05-22 11:51:01 +0100

    added deploy script for jump server with template file

---
+++ config/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TEMPLATE=../.github/workflows/template.yaml
+STACK_NAME=jump-server
+
+echo Linting template...
+echo
+cfn-lint $TEMPLATE
+
+# "org-vpc"
+VPC_ID=vpc-0507b30241a1b5d56
+# "private-org-opsSubnet1"
+SUBNET=subnet-0d9541b56ee0d37e6
+
+echo "Subnet = $SUBNET"
+echo "VpcId = $VPC_ID"
+echo
+
+echo Deploying Stack $STACK_NAME...
+
+aws cloudformation deploy \
+        --stack-name "$STACK_NAME" \
+        --template-file "$TEMPLATE" \
+        --parameter-overrides Subnet="$SUBNET" VpcId="$VPC_ID" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --no-fail-on-empty-changeset
+
+echo
+INSTANCE_ID=$(get_cfn_output "$STACK_NAME" 'InstanceId')
+echo "Jump server running as instance $INSTANCE_ID"
+echo
