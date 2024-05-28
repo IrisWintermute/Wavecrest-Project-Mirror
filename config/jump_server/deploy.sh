656c49435b8777f31346f1ff067f7c6965be3b1e
James <james.gray@wavecrest.com>
2024-05-28 11:35:17 +0100

    used list-stacks instead

--- config/jump_server/deploy.sh
+++ config/jump_server/deploy.sh
@@ -7,7 +7,7 @@ STACK_NAME=jump-server
 ACCOUNT_ID=$1
 REGION=$2
 
-STACK_EXISTS=$(aws cloudformation describe-stack-instance --stack-set-name "$STACK_NAME" --stack-instance-account "$ACCOUNT_ID" --stack-instance-region "$REGION" --output text) || ""
+STACK_EXISTS=$(aws cloudformation list-stacks --filters Name=tag:Name,Values="$STACK_NAME" Name=instance-state-code,Values=16 --output text)
 echo "$STACK_EXISTS"
 if ["$STACK_EXISTS" == ""] then
     echo Linting template...
