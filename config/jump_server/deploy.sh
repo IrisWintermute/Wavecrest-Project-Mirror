6bf155fbad3b159b6c26cbf70dd86023a1ae0f29
Iris <tomas5french@gmail.com>
2024-05-28 15:08:16 +0100

    reimplemented conditional

--- config/jump_server/deploy.sh
+++ config/jump_server/deploy.sh
@@ -5,33 +5,32 @@ set -euo pipefail
 TEMPLATE="config/jump_server/template.yaml"
 STACK_NAME=jump-server
 
-
-# "org-vpc"
-VPC_ID=vpc-0507b30241a1b5d56
-# "private-org-opsSubnet1"
-SUBNET=subnet-0d9541b56ee0d37e6
-
-echo "Subnet = $SUBNET"
-echo "VpcId = $VPC_ID"
-echo
-
-echo Deploying Stack $STACK_NAME...
-
-aws cloudformation deploy \
-    --stack-name "$STACK_NAME" \
-    --template-file "$TEMPLATE" \
-    --parameter-overrides Subnet="$SUBNET" VpcId="$VPC_ID" \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --no-fail-on-empty-changeset
-
-echo
-echo "Jump server running"
-echo
-
-STACK_EXISTS=$(aws cloudformation list-stacks --output text)
-echo "checking if $STACK_NAME exists"
-echo "$STACK_EXISTS"
 if [ "$STACK_EXISTS" == "" ] ; then
+    # "org-vpc"
+    VPC_ID=vpc-0507b30241a1b5d56
+    # "private-org-opsSubnet1"
+    SUBNET=subnet-0d9541b56ee0d37e6
+    
+    echo "Subnet = $SUBNET"
+    echo "VpcId = $VPC_ID"
+    echo
+    
+    echo Deploying Stack $STACK_NAME...
+    
+    aws cloudformation deploy \
+        --stack-name "$STACK_NAME" \
+        --template-file "$TEMPLATE" \
+        --parameter-overrides Subnet="$SUBNET" VpcId="$VPC_ID" \
+        --capabilities CAPABILITY_NAMED_IAM \
+        --no-fail-on-empty-changeset
+    
+    echo
+    echo "Jump server running"
+    echo
+    
+    STACK_EXISTS=$(aws cloudformation list-stacks --output text)
+    echo "checking if $STACK_NAME exists"
+    echo "$STACK_EXISTS"
 else
     echo "Stack $STACK_NAME already exists."
     echo
