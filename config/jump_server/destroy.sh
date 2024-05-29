e8b5952303574cec64bffd0422a26c29b6b07d58
Iris <tomas5french@gmail.com>
2024-05-29 09:52:00 +0100

    added space padding

--- config/jump_server/destroy.sh
+++ config/jump_server/destroy.sh
@@ -8,7 +8,7 @@ STACK_NAME=jump-server
 STACK_EXISTS=$(aws cloudformation list-stacks --output json --query "StackSummaries[?StackStatus == 'CREATE_COMPLETE'].StackName")
 echo "checking if $STACK_NAME exists"
 echo "$STACK_EXISTS"
-if ["${STACK_EXISTS[0]}" != ""]; then
+if [ "$STACK_EXISTS" != "" ]; then
     echo Destroying Stack $STACK_NAME...
 
     aws cloudformation delete-stack \
