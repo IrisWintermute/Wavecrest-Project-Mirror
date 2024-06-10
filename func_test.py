62438c1e113a41717509de276500c4562be47344
Iris <tomas5french@gmail.com>
2024-06-10 09:25:13 +0100

    updated func_test

--- func_test.py
+++ func_test.py
@@ -2,13 +2,14 @@ from lib import *
 
 def get_test_data():
     with open("test_data.txt", "r") as f:
-        return f.read().split(",")
+        return f.read()
     
-d = get_test_data()
-out = preprocess(d)
+d_raw = get_test_data()
+d = [[attribute for attribute in record.split(",")].append(0) for record in d_raw.split("\n")]
+out = [preprocess(r) for r in d]
 print(out)
 #print(len(out))
-out_2 = vectorise([out])
+out_2 = vectorise(out)
 print(out_2)
-out_3 = normalise(out_2[0])
+out_3 = [normalise(r) for r in out_2]
 print(out_3)
