0f9dc327b3aa83627de24aff05deaba03802d8c6
James <james.gray@wavecrest.com>
2024-06-21 10:16:52 +0100

    changed run to work, allowed for better ami to be run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3.10 flow.py
 
-sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key output_data --body output_data
+sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key output_data.txt --body output_data.txt
