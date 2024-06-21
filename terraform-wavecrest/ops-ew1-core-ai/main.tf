83c351a2fb6c8e7a06ccf890041bbb8e39675bfb
James <james.gray@wavecrest.com>
2024-06-21 17:16:59 +0100

    debugging

--- terraform-wavecrest/ops-ew1-core-ai/main.tf
+++ terraform-wavecrest/ops-ew1-core-ai/main.tf
@@ -75,7 +75,7 @@ module "app" {
   # public_zone_name = "${local.envname}.network.wavecrest.com" # Only needed if not this default
 
   #Note ami needs to include wildcards here, if needed - otherwise it will look for a specific AMI
-  ami                   = "amzn2-ami-hvm-c6g.2xlarge-arm64-gp2"
+  ami                   = "amzn2-ami-hvm-c6g*-arm64-gp2"
   instance_architecture = "arm64" # or arm64
 
   enable_weekdays_scale_down = false # Scale down at 6pm on weeknights
