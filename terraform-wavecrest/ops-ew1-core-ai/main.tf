da37d10cd08d0221a88f3d11548bdc3538184297
James <james.gray@wavecrest.com>
2024-06-24 11:49:26 +0100

    debugging

--- terraform-wavecrest/ops-ew1-core-ai/main.tf
+++ terraform-wavecrest/ops-ew1-core-ai/main.tf
@@ -59,7 +59,7 @@ module "app" {
     aws s3api get-object --bucket wavecrest-terraform-ops-ew1-ai --key exp_odine_u_332_p_1_e_270_20240603084457.csv.zip exp_odine_u_332_p_1_e_270_20240603084457.csv.zip
     unzip -o exp_odine_u_332_p_1_e_270_20240603084457.csv.zip
     sed -i '1d' exp_odine_u_332_p_1_e_270_20240603084457.csv
-    mv exp_odine_u_332_p_1_e_270_20240603084457.csv AI-Project/data/cdr.csv
+    mv exp_odine_u_332_p_1_e_270_20240603084457.csv AI-Project/main/data/cdr.csv
     rm exp_odine_u_332_p_1_e_270_20240603084457.csv.zip
 
     cd AI-Project
