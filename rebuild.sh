b77b8119fe2c4ecb8333d5a6b80bc83f39ebf77c
Iris <tomas5french@gmail.com>
2024-06-25 15:07:32 +0100

    added rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,6 @@
+cd ..
+mv AI-Project/main/data/cdr.csv cdr.csv
+rm -rf AI-Project
+git clone https://AI-Project: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@github.com/Wavecrest/AI-Project.git
+mv cdr.csv AI-Project/main/data/cdr.csv
+cd AI-Project
