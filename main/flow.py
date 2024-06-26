da1e51d89aaa07dfd610480146279c7c415fbd8a
Iris <tomas5french@gmail.com>
2024-06-26 16:27:45 +0100

    test

--- main/flow.py
+++ main/flow.py
@@ -16,7 +16,7 @@ def main():
             csv_list = [sanitise_string(record) for record in f.readlines(mx)]
     print(f"CDR data ({len(csv_list)} records) loaded.")
 
-    data_array = np.asarray([np.fromstring(record, dtype=object, sep=",") for record in csv_list], dtype=np.ndarray)
+    data_array = np.asarray([np.fromstring(record, dtype=str, sep=",") for record in csv_list], dtype=np.ndarray)
 
     # enrich and truncate records to optimise for clustering and fraud detection
     data_array_preprocessed = np.array([preprocess(record) for record in data_array], dtype=object)
