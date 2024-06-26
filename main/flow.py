e028c37cb268d5e67afff73e19a06b43b732badf
Iris <tomas5french@gmail.com>
2024-06-26 14:28:35 +0100

    test

--- main/flow.py
+++ main/flow.py
@@ -16,7 +16,7 @@ def main():
             csv_list = f.readlines(mx)
     print(f"CDR data ({len(csv_list)} records) loaded.")
 
-    data_array = np.array([np.array(sanitise_string(record).split(","), dtype="S32") for record in csv_list], dtype=object)
+    data_array = np.array([np.array(sanitise_string(record).split(","), dtype="O") for record in csv_list], dtype=np.array)
 
     # enrich and truncate records to optimise for clustering and fraud detection
     data_array_preprocessed = np.array([preprocess(record) for record in data_array], dtype=object)
