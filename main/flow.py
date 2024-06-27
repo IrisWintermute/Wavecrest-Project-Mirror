f7912cabc577fb8c41b5efceef8d8978320163d7
Iris <tomas5french@gmail.com>
2024-06-27 14:51:20 +0100

    testing

--- main/flow.py
+++ main/flow.py
@@ -17,8 +17,9 @@ def main():
     print(f"CDR data ({len(csv_list)} records) loaded.")
 
     # data_array = np.asarray([np.asarray(sanitise_string(record).split(","), dtype=object) for record in csv_list], dtype=np.ndarray)
-    to_record = lambda s: np.array(sanitise_string(s).split(","))
-    data_array = np.asarray(list(map(to_record, csv_list)))
+    to_record = np.array(lambda s: sanitise_string(s).split(","), dtype=object)
+    # data_array = np.asarray(list(map(to_record, csv_list)))
+    data_array = np.apply_along_axis(to_record, 0, np.array(csv_list))
     print(data_array)
 
     # enrich and truncate records to optimise for clustering and fraud detection
