165298f71f005a5833883ca6c1e269058562ba26
Iris <tomas5french@gmail.com>
2024-06-27 15:52:00 +0100

    test

--- main/test_script.py
+++ main/test_script.py
@@ -2,7 +2,10 @@
 # importing library
 import numpy as np
  
-lst = ["a,f,h,ueq,r,w","d,t,joy,u,igd,d,","q,t,io,p,f,"]
+lst = ["a,f,h,ueq,r,w","d,t,joy,u,igd,d","q,t,io,p,f,y"]
 to_r = lambda s: s.split(",")
 nlst = list(map(to_r, lst))
 print(nlst)
+data_array = np.array(nlst)
+print(data_array)
+print(data_array.ndim)
