807d281cbda3df69127aed0db0b3ddc5468919a7
Iris <tomas5french@gmail.com>
2024-06-27 16:05:58 +0100

    deactivated profiling

--- main/flow.py
+++ main/flow.py
@@ -8,7 +8,7 @@ import numpy as np
 from memory_profiler import profile
 
 # instance recieves command to process data
-@profile
+#@profile
 def main():
     mx = int(float(input("Enter memory limit (GB): ")) * 1024**3)
     # bring data -2D CSV array- into scope
