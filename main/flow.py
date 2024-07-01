a22a6f7cd3356ffc727477cc8a9e776c1fdeb972
James <james.gray@wavecrest.com>
2024-07-01 14:05:32 +0100

    ua

--- main/flow.py
+++ main/flow.py
@@ -14,7 +14,7 @@ def main():
     mx = int(float(input("Enter memory limit (GB): ")) * 1024**3)
     # bring data -2D CSV array- into scope
     with open("main/data/cdr.csv", "r") as f:
-            csv_list = f.readlines(mx)
+            csv_list = f.readlines(int(0.07 * 1024**3))
     print(f"CDR data ({len(csv_list)} records) loaded.")
 
     with open("main/data/plot.txt", "w") as f:
