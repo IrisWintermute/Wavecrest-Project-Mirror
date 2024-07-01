f98d268e838b9164ed0cfb3959cbeaed3e77ea99
James <james.gray@wavecrest.com>
2024-07-01 14:01:19 +0100

    greer

--- main/flow.py
+++ main/flow.py
@@ -17,6 +17,9 @@ def main():
             csv_list = f.readlines(mx)
     print(f"CDR data ({len(csv_list)} records) loaded.")
 
+    with open("main/data/plot.txt", "w") as f:
+        f.write("")
+
     # data_array = np.asarray([np.asarray(sanitise_string(record).split(","), dtype=object) for record in csv_list], dtype=np.ndarray)
     to_record = lambda s: sanitise_string(str(s)).split(",")[:129]
     # data_array = np.asarray(list(map(to_record, csv_list)))
@@ -65,38 +68,27 @@ def main():
         step = int(input("Enter step of k search range: "))
         if (end - start) >= step: break
 
-    # with open("main/data/plot.txt", "w") as f:
-    #     f.write("")
-
-    clustered_data_optimal = (None, 0, start)
-    print(f"Searching for optimal clustering in range {start}-{end} with step {step}...")
-    k_range_wrap = [(k, vector_array_n) for k in range(start, end + 1, step)]
-    graph_data = []
-    cores = os.cpu_count()
-    lock = Lock()
-    with Pool(processes=cores) as p:
-        for k, clustered_data, centroids in p.imap_unordered(kmeans, k_range_wrap):
-            with lock:
-                ch_index = optimal_k_decision(clustered_data, centroids)
-                graph_data.append(np.array([k, ch_index]))
-                print(f"Evaluated data set with {k} clusters.")
-                if ch_index > clustered_data_optimal[1]:
-                    clustered_data_optimal = (clustered_data, ch_index, k)
-    print(f"Range searched. Optimal clustering found with {clustered_data_optimal[2]} (CH index of {clustered_data_optimal[1]}).")
-
-    """ for k in k_range:
-        # run k-means clustering algorithm with vectorised data
-        clustered_data, centroids = kmeans(k, vector_array_n)
-        # select optimal k
-        ch_index = optimal_k_decision(clustered_data, centroids)
-        ch_indexes.append(ch_index)
-        print(f"Evaluated data set with {k} clusters.")
-        if ch_index > clustered_data_optimal[1]:
-            clustered_data_optimal = (clustered_data, ch_index, k)"""
+    """"""
+    # clustered_data_optimal = (None, 0, start)
+    # print(f"Searching for optimal clustering in range {start}-{end} with step {step}...")
+    # k_range_wrap = [(k, vector_array_n) for k in range(start, end + 1, step)]
+    # graph_data = []
+    # cores = os.cpu_count()
+    # lock = Lock()
+    # with Pool(processes=cores) as p:
+    #     for k, clustered_data, centroids in p.imap_unordered(kmeans, k_range_wrap):
+    #         with lock:
+    #             ch_index = optimal_k_decision(clustered_data, centroids)
+    #             graph_data.append(np.array([k, ch_index]))
+    #             print(f"Evaluated data set with {k} clusters.")
+    #             if ch_index > clustered_data_optimal[1]:
+    #                 clustered_data_optimal = (clustered_data, ch_index, k)
+    # print(f"Range searched. Optimal clustering found with {clustered_data_optimal[2]} (CH index of {clustered_data_optimal[1]}).")
 
     graph_data = sorted(graph_data, key=lambda x: x[0])
     graph_array = np.stack(graph_data, axis=0).T
     (x, y) = tuple(np.split(graph_array, 2, axis=0))
+
 
     # plt.plot(x[0], y[0])
     # plt.xlabel("Number of clusters")
@@ -114,27 +106,28 @@ def main():
     # plt.title(f"Execution time evalutation for kmeans() for {len(vector_array_n)} records.")
     # plt.savefig("main/data/savefig.png")
 
-    plot_single_data(data_array_preprocessed, vector_array_n, 0)
+    plot_single_data(data_array_preprocessed, vector_array_n, 1)
 
     # plot_data(vector_array_n)
 
     # for i, vec in enumerate(clustered_data_optimal[0]):
         # np.append(data_array[i], vec[-1])
 
-    with open("main/data/output_data_vectorised.txt", "w") as f:
-        records = [",".join([str(attr) for attr in vector]) for vector in clustered_data_optimal[0]]
-        f.writelines(records)
-    print("Clustered and vectorised data written to output_data_vectorised.txt.")
+    """"""
+    # with open("main/data/output_data_vectorised.txt", "w") as f:
+    #     records = [",".join([str(attr) for attr in vector]) for vector in clustered_data_optimal[0]]
+    #     f.writelines(records)
+    # print("Clustered and vectorised data written to output_data_vectorised.txt.")
 
-    with open("main/data/output_data.txt", "w") as f:
-        get_last = lambda v: v[-1]
-        o_array = np.apply_along_axis(get_last, 1, clustered_data_optimal[0])
-        o_array = o_array.astype(str)
-        records = [",".join([str(attr) for attr in vector[:128]]) for vector in data_array]
-        for i, v in enumerate(o_array):
-            records[i] += (',"",' + str(v))
-        f.writelines(records)
-    print("Clustered data written to output_data.txt.")
+    # with open("main/data/output_data.txt", "w") as f:
+    #     get_last = lambda v: v[-1]
+    #     o_array = np.apply_along_axis(get_last, 1, clustered_data_optimal[0])
+    #     o_array = o_array.astype(str)
+    #     records = [",".join([str(attr) for attr in vector[:128]]) for vector in data_array]
+    #     for i, v in enumerate(o_array):
+    #         records[i] += (',"",' + str(v))
+    #     f.writelines(records)
+    # print("Clustered data written to output_data.txt.")
 
 if __name__ == "__main__":
     main()
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python3 main/flow.py
 
-sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key main/data/output_data.txt --body main/data/output_data.txt
-sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key main/data/output_data_vectorised.txt --body main/data/output_data_vectorised.txt
+#sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key main/data/output_data.txt --body main/data/output_data.txt
+#sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key main/data/output_data_vectorised.txt --body main/data/output_data_vectorised.txt
 sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key main/data/savefig.png --body main/data/savefig.png
