7810f31c3246855e2b86af57bd6468b02a83a74b
Iris <tomas5french@gmail.com>
2024-07-05 12:16:22 +0100

    test

--- main/flow.py
+++ main/flow.py
@@ -116,5 +116,5 @@ def main(plot = 0):
     print("Clustered data written to output_data.txt.")
 
 if __name__ == "__main__":
-    plot_preprocessed_data = 2
+    plot_preprocessed_data = 1
     main(plot_preprocessed_data)
