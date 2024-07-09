4c4254d5f993d3ed5792cb4e8dbade938aa65a12
Iris <tomas5french@gmail.com>
2024-07-09 12:43:37 +0100

    test

--- main/lib.py
+++ main/lib.py
@@ -67,7 +67,7 @@ def plot_clustered_data_3d(clustered_data):
     cx.set_ylabel('Dimension')
     cx.set_zlabel('Normalised Frequency')
     cx.view_init(elev=20., azim=-35)
-    cx.set_position([0.1,0.1,0.1,0.1])
+    cx.set_position([0.1,0.1,1,1])
 
     dx.set_xlim(1, 0)
     dx.set_ylim(0, clustered_data.shape[1] - 2)
@@ -75,7 +75,7 @@ def plot_clustered_data_3d(clustered_data):
     dx.set_xlabel('Range')
     dx.set_ylabel('Dimension')
     dx.view_init(elev=20., azim=35)
-    dx.set_position([0.6,0.6,0.1,0.1])
+    dx.set_position([0.6,0.1,1,1])
     
     ax.xaxis.tick_top()
     ax.set_xlabel('Dimension')
