709daf9fc777fc73fd1f0cdb96c77ec0bb18dab3
Iris <tomas5french@gmail.com>
2024-07-14 19:03:53 +0100

    test

--- main/lib.py
+++ main/lib.py
@@ -169,7 +169,7 @@ def plot_clustered_data_batch(clustered_data):
                     x_p, y_p = x[o_array == k], y[o_array == k]
                     plt.scatter(x_p, y_p, color=colors[k % len(colors)])
                 f = f"main/data/savefig_batch/{dims[i]}_{dims[j]}.png"
-                plt.legend([f"cluster {l}" for l in range(n)])
+                plt.legend([f"cluster {l}" for l in range(n)], loc="upper right", bbox_to_anchor=(1.5,1))
                 plt.savefig(f)
                 filenames.append(f)
 
