10e02a81b18e5cd2d1f621ceb7adbc9279266521
Iris <tomas5french@gmail.com>
2024-07-19 12:40:32 +0100

    correction

--- main/func_test.py
+++ main/func_test.py
@@ -172,7 +172,7 @@ def graph_test_assignments():
     ax.set_ylabel("Assignment accuracy % (relative to clustering, 10% of input data)")
     plt.legend(["With vertical weighting", "Without vertical weighting"])
     plt.savefig("savefig.png")
-    subprocess.run(["sudo aws s3api put-object --bucket wavecrest-terraform-ops-ew1-ai --key savefig.png --body savefig.png"])
+    subprocess.run(["sudo", "aws", "s3api", "put-object", "--bucket", "wavecrest-terraform-ops-ew1-ai", "--key", "savefig.png", "--body", "savefig.png"])
 
 if __name__ == "__main__":
     graph_test_assignments()
