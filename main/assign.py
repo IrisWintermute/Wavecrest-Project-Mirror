5d2b54bcc8d5ef83aadf6db3be555257924bc3fc
Iris <tomas5french@gmail.com>
2024-07-21 21:53:06 +0100

    laid out control flow in assign.py

--- main/assign.py
+++ main/assign.py
@@ -1,13 +1,22 @@
 import numpy as np
 from lib import *
 
+def main():
+    raw_record = get_record()
 
-with open("incoming_records.txt", "r") as f:
-#     records_raw = f.readlines()
-#     preprocessed_records = list(map(preprocess_single, records_raw))
-    preprocessed_records = f.readlines()
+    # needs to use values_dump generated from dataset preprocessing
+    preprocessed_record = preprocess_incoming_record(raw_record)
 
-(centroids, stdevs) = get_clustering_parameters()
+    (centroids, stdevs) = get_clustering_parameters()
 
-assigned_records = np.array([assign_cluster(record, centroids, stdevs) for record in preprocessed_records])
+    # cluster indexes as keys, fraud ratings as values
+    fraud_hash = rate_cluster_fraud(centroids, stdevs)
 
+    assigned_record = assign_cluster(preprocessed_record, centroids, stdevs)
+
+    rating = fraud_hash.get(assigned_record[-1])
+
+    return rating
+
+if __name__ == "__main__":
+    main()
--- main/func_test.py
+++ main/func_test.py
@@ -154,11 +154,11 @@ def graph_test_assignments():
         _, out, cs = kmeans((4, vector_array_n))
 
         x = [v * 0.025 for v in range(25, 61)] * 10
-        y = [test_assignments(out, cs, a) for a in x]
-        ax.scatter(x, y)
-    ax.set_xlabel("Value of exp. factor applied to n.d. magnitude")
+        # y = [test_assignments(out, cs, a) for a in x]
+        # ax.scatter(x, y)
+    # ax.set_xlabel("Value of exp. factor applied to n.d. magnitude")
     ax.set_ylabel("Assignment accuracy % (relative to clustering, 10% of input data)")
-    plt.title("Accuracy over exp. factor range, 0.1GB records, 4 clusters")
+    # plt.title("Accuracy over exp. factor range, 0.1GB records, 4 clusters")
     plt.legend(["0.1GB", "0.5GB", "1.0GB"])
     plt.savefig("savefig.png")
     subprocess.run(["sudo", "aws", "s3api", "put-object", "--bucket", "wavecrest-terraform-ops-ew1-ai", "--key", "savefig.png", "--body", "savefig.png"])
