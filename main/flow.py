0856bb656961e9492e07cb44404157c1b8d04a8a
James <james.gray@wavecrest.com>
2024-07-22 09:13:53 +0100

    testing memory

--- main/flow.py
+++ main/flow.py
@@ -16,8 +16,50 @@ def main(plot = 0):
         mxg = sys.argv[2]
     else:
         mxg = input("Enter memory limit (GB): ")
+    mx = int(float(mxg) * 1024**3)
+    # bring data -2D CSV array- into scope
+    size = os.path.getsize("main/data/cdr.csv")
+    filestep = size // mx if size // mx >= 1 else 1
+    with open("main/data/cdr.csv", "r", encoding="utf-8") as f:
+            # systematic sampling of dataset
+            csv_list_r = f.readlines(size)
+            csv_list = csv_list_r[::filestep]
+            del csv_list_r
+            del mx
+    print(f"CDR data ({len(csv_list)} records) loaded.")
 
-    vector_array_n = get_preprocessed_data(mxg)
+   """ with open("main/data/plot.txt", "w") as f:
+        f.write("")
+
+    # data in csv has row length of 129
+    to_record = lambda s: sanitise_string(str(s)).split(",")[:25]
+    csv_nested_list = list(map(to_record, csv_list))
+    del csv_list, to_record
+    data_array = np.array(csv_nested_list, dtype=object)
+    del csv_nested_list
+
+    # enrich and truncate records to optimise for clustering and fraud detection
+
+    # data_array_preprocessed = np.apply_along_axis(preprocess, 1, data_array)
+    # del data_array
+    # print("Data preprocessed.")
+
+    data_array_loaded = load_attrs(data_array)
+    del data_array
+    data_array_preprocessed = np.apply_along_axis(preprocess_n, 0, data_array_loaded)
+    del data_array_loaded
+    print("Data preprocessed.")
+
+    # (vectorise) convert each record to array with uniform numerical type - data stored as nested array
+    with open("main/data/values_dump.txt", "w") as f:
+        f.write("")
+    vector_array = np.apply_along_axis(vectorise, 0, data_array_preprocessed)
+    del data_array_preprocessed
+    print("Data vectorised.")  
+
+    vector_array_n = np.apply_along_axis(normalise, 0, vector_array)
+    del vector_array
+    print("Data normalised.")
 
     if plot == 1:
         # plot_single_data(data_array_preprocessed, vector_array_n, 3)
@@ -41,12 +83,12 @@ def main(plot = 0):
             if (end - start) >= step: break
 
     cluster_data = (None, None, 0, start)
+    print(f"Searching for optimal clustering in range {start}-{end} with step {step}...")
+    k_range_wrap = [(k, vector_array_n) for k in range(start, end + 1, step)]
+    del vector_array_n
     graph_data = []
     cores = os.cpu_count()
     lock = Lock()
-    print(f"Searching for optimal clustering in range {start}-{end} with step {step}...")
-    k_range_wrap = [(k, vector_array_n) for k in range(start, end + 1, step)]
-
     with Pool(processes=cores) as p:
         for k, clustered_data, centroids in p.imap_unordered(kmeans, k_range_wrap):
             with lock:
@@ -71,13 +113,22 @@ def main(plot = 0):
             f.write(str(cluster_data[0].shape[0]) + "," + ",".join(sizes) + "\n")
         return 0
 
+    # with open("main/data/plot.txt", "r") as f:
+    #     y = f.read().split(",")[1:]
+    # y = [float(v) for v in y]
+    # x = [v for v in range(start, end + 1, step)]
+    # plt.plot(x, y, "r-")
+    # plt.xlabel("Number of clusters")
+    # plt.ylabel("Execution time (s)")
+    # plt.title(f"Execution time evalutation for kmeans() for {len(vector_array_n)} records.")
+    # plt.savefig("main/data/savefig.png")
+
     if plot == 3:
         with open("main/data/output_data_vectorised.txt", "w") as f:
             records = [",".join([str(attr) for attr in vector]) for vector in cluster_data[0]]
             f.writelines(records)
         print("Clustered and vectorised data written to output_data_vectorised.txt.")
 
-        data_array = get_raw_data(mxg)
         with open("main/data/output_data.txt", "w") as f:
             o_array = o_array.astype(str)
             records = [",".join([str(attr) for attr in vector[:128]]) for vector in data_array]
@@ -88,7 +139,7 @@ def main(plot = 0):
 
     if plot == 0:
         save_clustering_parameters(cluster_data[1], cluster_data[0], o_array)
-
+"""
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
