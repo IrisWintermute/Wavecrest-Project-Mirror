fe46cc817b1d2cbe7fcc3303e8e0e0d6d8c7bb0a
Iris <tomas5french@gmail.com>
2024-07-22 11:59:34 +0100

    added function logic for assign.py

--- main/assign.py
+++ main/assign.py
@@ -10,12 +10,13 @@ def main():
     (centroids, stdevs) = get_clustering_parameters()
 
     # cluster indexes as keys, fraud ratings as values
-    fraud_hash = rate_cluster_fraud(centroids, stdevs)
+    fraud_hash = rate_cluster_fraud(stdevs)
 
     assigned_record = assign_cluster(preprocessed_record, centroids, stdevs)
 
     rating = fraud_hash.get(assigned_record[-1])
 
+    print(f"Record assigned to cluster with index {assigned_record[-1]} with fraudulence rating of {rating} / 1.0")
     return rating
 
 if __name__ == "__main__":
