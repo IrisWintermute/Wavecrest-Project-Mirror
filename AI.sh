61484b4101a003456ad159cc89be317242b4d78b
Iris <tomas5french@gmail.com>
2024-07-25 15:17:33 +0100

    corr

--- AI.sh
+++ AI.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[{$1} == "start"]]; then
+if [["{$1}" == "start"]]; then
     git pull
     python3 main/manager.py
 fi
-if [[{$1} == "kill"]]; then
+if [["{$1}" == "kill"]]; then
     kill 15 $(pidof python3)
 fi
-if [[{$1} == "reload"]]; then
+if [["{$1}" == "reload"]]; then
     kill 1 $(pidof python3)
 fi
