a98a4b9311bd84a97ec0850a9326f0270d351532
Iris <tomas5french@gmail.com>
2024-07-26 11:47:51 +0100

    corr

--- main/reload.sh
+++ main/reload.sh
@@ -5,6 +5,6 @@ rm -f "AI-Project/main/data/$arg.csv.zip"
 aws s3api get-object --bucket wavecrest-terraform-ops-ew1-ai --key "$arg.csv.zip" "$arg.csv.zip"
 unzip -o "$arg.csv.zip"
 sed -i '1d' "$arg.csv"
-mv "$arg.csv AI-Project/main/data/cdr.csv"
+mv "$arg.csv" AI-Project/main/data/cdr.csv
 rm -f "$arg.csv.zip"
 echo "$arg.csv loaded."
