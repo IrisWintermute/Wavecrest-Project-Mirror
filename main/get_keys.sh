53b2055512d98a4f087b2d880052f08c74d3332d
Iris <tomas5french@gmail.com>
2024-07-26 09:49:39 +0100

    corrected file creation

--- main/get_keys.sh
+++ main/get_keys.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash#!/usr/bin/env bash
 
+touch cache.txt
+chmod +rw cache.txt
 aws s3api list-objects --bucket wavecrest-terraform-ops-ew1-ai --prefix exp_odine --query 'Contents[].{Key: Key}' > cache.txt
