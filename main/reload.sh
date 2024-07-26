6b1be622e0e7755e164e108a2b556d3a8f2e6731
Iris <tomas5french@gmail.com>
2024-07-26 12:29:39 +0100

    removing sed byproduct files with grep

--- main/reload.sh
+++ main/reload.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 arg=$1
 echo "$arg.csv loading..."
-rm -f "AI-Project/main/data/cdr.csv"
 aws s3api get-object --bucket wavecrest-terraform-ops-ew1-ai --key "$arg.csv.zip" "$arg.csv.zip"
 unzip -o "$arg.csv.zip"
 sed -i '1d' "$arg.csv"
+rm -f "AI-Project/main/data/cdr.csv"
 mv "$arg.csv" AI-Project/main/data/cdr.csv
 rm -f "$arg.csv.zip"
+rm -f $(find | grep "sed")
 echo "$arg.csv loaded."
