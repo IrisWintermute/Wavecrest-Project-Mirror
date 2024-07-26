1d9a88c539c1ecbb01fd66f89a33e01e6313ebf0
Iris <tomas5french@gmail.com>
2024-07-26 09:46:26 +0100

    corrected date extraction

--- main/get_keys.sh
+++ main/get_keys.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash#!/usr/bin/env bash
 
-$(aws s3api list-objects --bucket wavecrest-terraform-ops-ew1-ai --prefix exp_odine --query 'Contents[].{Key: Key}') > cache.txt
+aws s3api list-objects --bucket wavecrest-terraform-ops-ew1-ai --prefix exp_odine --query 'Contents[].{Key: Key}' > cache.txt
