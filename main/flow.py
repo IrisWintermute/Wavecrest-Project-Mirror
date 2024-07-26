9550a4b764bc51d006e558a1e58f87205593ee3e
Iris <tomas5french@gmail.com>
2024-07-26 10:17:45 +0100

    test

--- main/flow.py
+++ main/flow.py
@@ -14,7 +14,7 @@ import time
 # @profile
 def main(plot = 0, mxg = 0, start = 0, end = 0, step = 0):
     # get latest cdr data from s3
-    subprocess.run(["bash", "get_keys.sh"])
+    subprocess.run(["./get_keys.sh"])
     with open("cache.txt", "r") as f:
         l = f.read()
     print(l)
