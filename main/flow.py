dd41ec9bd3c07b57e193be6f4cd651e7fcd97f3a
James <james.gray@wavecrest.com>
2024-07-29 11:11:04 +0100

    and my life

--- main/flow.py
+++ main/flow.py
@@ -70,12 +70,12 @@ def main(plot = 0, mxg = 0, start = 0, end = 0, step = 0):
 
     del csv_list
 
-    counter_start = time.perf_counter()
-    data_array = np.array(csv_nested_list, dtype='U15')
+    """counter_start = time.perf_counter()
+    data_array = np.array(csv_nested_list, dtype=object)
     counter_stop = time.perf_counter()
     print(f"converting list to array took {counter_stop - counter_start} seconds")
 
-    """del csv_nested_list
+    del csv_nested_list
 
     # enrich and truncate records to optimise for clustering and fraud detection
 
