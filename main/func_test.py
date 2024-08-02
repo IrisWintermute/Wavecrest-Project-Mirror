c809243e282143b713ec688f1b2f8c24329fae05
Iris <tomas5french@gmail.com>
2024-08-02 14:11:04 +0100

    stupid

--- main/func_test.py
+++ main/func_test.py
@@ -210,7 +210,7 @@ async def tcp_echo_client(message):
     await writer.drain()
 
     data = await reader.read(100)
-    print(f'Received: {data.decode():.2f}')
+    print(f'Received: {data.decode()}')
 
     print('Close the connection')
     writer.close()
